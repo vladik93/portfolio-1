abf9b17f968d711ce0e28a7719c3aa7723da82d3
Vladi <39379713+vladik93@users.noreply.github.com>
2024-03-07 11:41:07 +0200

    making the profile-image move on scroll

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log('script.js is running');
 
 const headerEl = document.getElementById('header');
+const profileWrapperEl = document.getElementById('profile-wrapper');
 
 let headerOffsetTop = headerEl.offsetTop;
 
@@ -15,3 +16,25 @@ window.addEventListener('scroll', (e) => {
     headerEl.classList.remove('fixed');
   }
 });
+
+// ABOUT 
+
+let  profileOffsetTop = profileWrapperEl.offsetTop - 30;
+
+
+window.addEventListener('scroll', () => {
+  let x = 0;
+
+  if(window.scrollY > profileOffsetTop) {
+    console.log(scrollY);
+    x += 10;
+    profileWrapperEl.style.backgroundPositionY = x + "%";
+  } else {
+    x -= 10;
+    profileWrapperEl.style.backgroundPositionY = x + "%";
+  }
+})
+
+
+
+
