f4e4d4d63f89e75502f92d600aeb4954583d1aa2
Vladi <39379713+vladik93@users.noreply.github.com>
2024-03-07 08:58:01 +0200

    fixed sticky header

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('script.js is running');
+
+const headerEl = document.getElementById('header');
+
+let headerOffsetTop = headerEl.offsetTop;
+
+
+window.addEventListener('scroll', (e) => {
+  let scrollY = window.scrollY;
+
+  if(scrollY > headerOffsetTop) {
+    headerEl.classList.add('fixed');
+ 
+  } else {
+    headerEl.classList.remove('fixed');
+  }
+});
